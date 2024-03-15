b53df8bb1acf0ecf7d0fa98c25de52b9d9b16d13
Sarvesh Patil <sarveshp273@gmail.com>
2024-03-16 04:30:44 +0530

    Add base and homepage configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/React-OP-Template/",
   plugins: [react()],
   server: {
     port: 3000,
